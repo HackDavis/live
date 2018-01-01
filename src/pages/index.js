4475e27e3b3269c8c17e128e3403b0e1afecd302
klanmiko <eternity01033@gmail.com>
2017-12-31 20:42:03 -0800

    grid attempt

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Countdown from '../components/countdown.js'
 import Schedule from '../components/schedule.js'
 import {Timeline} from 'react-twitter-widgets'
 import Knob from 'react-time-knob'
+import './main.css'
 
 class IndexPage extends React.Component {
   constructor(props){
@@ -17,34 +18,28 @@ class IndexPage extends React.Component {
   }
   render(){
     return (
-      <div className="container-fluid" style={{paddingTop: "15px"}}>
-        <div className="large-container row">
-          <div className="col-lg-3 col-md-4 d-flex justify-content-center align-items-start px-3">
-            <div ref={(e) => {if(e) this.circleWidth = e.clientWidth - parseInt(window.getComputedStyle(e).getPropertyValue("padding-left")) - parseInt(window.getComputedStyle(e).getPropertyValue('padding-left'))}}
-            className="knob">
-              <Knob lineCap='round' 
-              sColor="#55419c" 
-              mColor="#945ebe"
-              hColor="#b766ad" 
-              width={this.circleWidth} 
-              height={this.circleWidth} 
-              hours={this.state.hours} 
-              minutes={this.state.minutes} 
-              seconds={this.state.seconds} 
-              displayNumber={window.innerWidth < 768} 
-              thickness={5} />
-            </div>
-          </div>
-          <div className="col-md-8 col-lg-5 countdown-container">
-            <Countdown hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds}/>
-          </div>
-          <div className="col-md-4 offset-md-3 offset-lg-0 px-4">
-            <div style={{marginTop: "10px", maxWidth: "600px"}}>
-              <Timeline dataSource={{sourceType: 'profile', screenName: 'hack_davis'}} options={{height: "600px"}}/>
-            </div>
-          </div>
+      <div className="container-fluid index-container" style={{paddingTop: "15px"}}>
+        <div ref={(e) => {if(e) this.circleWidth = e.clientWidth - parseInt(window.getComputedStyle(e).getPropertyValue("padding-left")) - parseInt(window.getComputedStyle(e).getPropertyValue('padding-left'))}}
+        className="knob">
+          <Knob lineCap='round' 
+          sColor="#55419c" 
+          mColor="#945ebe"
+          hColor="#b766ad" 
+          width={this.circleWidth} 
+          height={this.circleWidth} 
+          hours={this.state.hours} 
+          minutes={this.state.minutes} 
+          seconds={this.state.seconds} 
+          displayNumber={window.innerWidth < 768} 
+          thickness={5} />
         </div>
-      </div>
+        <div className="countdown-container">
+          <Countdown hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds}/>
+        </div>
+        <div className="timeline-container" style={{marginTop: "10px", maxWidth: "600px"}}>
+          <Timeline dataSource={{sourceType: 'profile', screenName: 'hack_davis'}} options={{height: "600px"}}/>
+        </div>
+      </div>        
     )
   }
 clock() {
