fa29b1d6495879f4b1ba0af984d823ce8a7d1e28
miguel-acevedo <acevedomiguel@outlook.com>
2018-01-14 12:20:55 -0800

    Centered the map image

--- src/pages/map.js
+++ src/pages/map.js
@@ -2,9 +2,27 @@ import React from 'react'
 import ZoomableImage from 'react-zoomable-image';
 
 const MAP = () => (
-  <div className="container">
-    <h1>Map</h1>
-  </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>>
+         <ZoomableImage
+           baseImage={{
+             alt: 'An image',
+             src: 'http://getschedulehelper.com/hackdavis/mapzoom.jpg',
+             width: 1150,
+             height: 900
+           }}
+           largeImage={{
+             alt: 'A large image',
+             src: 'http://getschedulehelper.com/hackdavis/mapzoom.jpg',
+             width: 1150,
+             height: 900
+           }}
+           thumbnailImage={{
+             alt: 'A small image',
+             src: 'http://getschedulehelper.com/hackdavis/mapzoom.jpg'
+           }}
+         />
+         </div>
+
 )
 
 export default MAP
