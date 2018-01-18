b914e23942935180325aad98ff1d9705c1bbf1b3
Miguel Acevedo <acevedomiguel@outlook.com>
2018-01-17 23:53:00 -0800

    Made map responsive

--- src/pages/map.js
+++ src/pages/map.js
@@ -2,9 +2,17 @@ import ImageZoom from 'react-medium-image-zoom'
 import React from 'react'
 import mappic from './mapzoom.jpg'
 
+var divStyle = {
+    height: 'auto',
+    width: 'auto',
+    height: 'calc(100vh - 100px)'
+};
+
 const MAP = () => (
 <div className="container">
-  <img src={mappic}/>
+  <div style={divStyle}>
+    <a href={"http://getschedulehelper.com/hackdavis/mapzoom.jpg"} target={"_blank"}><img id={"mymap"} src={mappic}/></a>
+  </div>
 </div>
 
 )
